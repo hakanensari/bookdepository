512e4ec79507d9355950e3d2dfc1d151e4f2e061
hakanensari <hakanensari@gmail.com>
2014-07-15 19:42:52 +0100

    Don't use angle brackets

--- test/helper.rb
+++ test/helper.rb
@@ -5,8 +5,8 @@ require_relative '../lib/bookdepository'
 VCR.configure do |c|
   c.hook_into :excon
   c.cassette_library_dir = 'test/vcr_cassettes'
-  c.filter_sensitive_data('<CLIENT_ID>') { ENV['BOOK_DEPOSITORY_CLIENT_ID'] }
-  c.filter_sensitive_data('<AUTH_KEY>') { ENV['BOOK_DEPOSITORY_AUTH_KEY'] }
+  c.filter_sensitive_data('CLIENT_ID') { ENV['BOOK_DEPOSITORY_CLIENT_ID'] }
+  c.filter_sensitive_data('AUTH_KEY') { ENV['BOOK_DEPOSITORY_AUTH_KEY'] }
   c.default_cassette_options = {
     record: :none,
     match_requests_on: [:method,
